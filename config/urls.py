7d7801cc01a86992661f4316310151c8be6ba108
fst <fst@local>
2026-07-13 08:13:30 +0000

    Add static file serving for DEBUG dev mode
    Append static() URL pattern so /static/ assets (built Vue SPA)
    are served by Django's dev server. Without this, the SPA root
    loads but its JS/CSS return 404 in DEBUG=True. This is a dev-only
    convenience; production (DEBUG=False) static serving is tracked
    separately (see issue).

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ URL configuration for Presence Board project.
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from .views import IndexView
 
@@ -22,5 +24,5 @@ urlpatterns = [
 
     # フロントエンド SPA エントリーポイント (フォールバック)
     re_path(r'^.*$', IndexView.as_view(), name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
